1921c60a6a90e7c0c6eff8652844f6161c08d774
salmasiddiqha <62383829+salmasiddiqha@users.noreply.github.com>
2021-10-12 14:25:00 +0530

    Create python.py

---
+++ python.py
@@ -0,0 +1,15 @@
+import os
+List1 = [8,9,3,6,1,10]
+List1.reverse()
+print("The reverse list is",List1)
+
+List2 = [91,67,120,34,76,87,56,64,345]
+list2.sort()
+print("The sorted list is ", List2)
+
+List3 = []
+List3 = List1.copy()
+print("list3 =", List3)
+
+indexvalue = List2[2:6]
+print("The index values are" , indexvalue)
